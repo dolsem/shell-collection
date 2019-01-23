059f895341111d0c56fb45b9b942ed134bbd7b29
Denis Semenenko <dols3m@gmail.com>
2019-01-23 17:39:17 -0500

    Added a script to download utilities

---
+++ source_utils.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+###########################################################################
+# Script Name	: source_utils.bash
+# Description	: Used to store local copy of utils for sourcing.
+# Author      : Denis Semenenko
+# Email       : dols3m@gmail.com
+# Date written: September 2018
+#
+# Distributed under MIT license
+# Copyright (c) 2019 Denis Semenenko
+###########################################################################
+
+PREFIX='[Dolsem Bash Utils]'
+REPO_URL='https://github.com/dolsem/bash-utils'
+BASE_URL="${REPO_URL}/raw/master"
+CACHE_DIR="$(dirname $0)/.bash-utils"
+
+clone_repo() {
+  echo "${PREFIX} Getting all utilities..."
+  git clone $REPO_URL "${CACHE_DIR}"
+  if [[ ! $? -eq 0 ]]; then
+    exit $?
+  fi
+}
+
+download_one() {
+  echo "${PREFIX} Getting $1 utilities..."
+  mkdir -p "$CACHE_DIR"
+  if cd "$CACHE_DIR"; then
+    wget -O "$1.bash" "${BASE_URL}/$1.bash" -q --show-progress --progress=bar:noscroll
+    cd - 1>/dev/null
+  fi
+  if [[ ! $? -eq 0 ]]; then
+    exit $?
+  fi
+}
+
+if [[ -z $util ]]; then
+  clone_repo
+  source "${CACHE_DIR}/*.bash"
+else
+  download_one $util
+  source "${CACHE_DIR}/${util}.bash"
+fi
+
+if [[ $? -eq 0 ]]; then
+  echo "${PREFIX} Done."
+else
+  exit $?
+fi
