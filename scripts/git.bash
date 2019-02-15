ea7fb34caea25f0c9e096fb97476b4f51d308a62
Denis Semenenko <dols3m@gmail.com>
2019-02-15 17:15:01 -0500

    Rewrote git retrospect from scratch - new behavior

--- scripts/git.bash
+++ scripts/git.bash
@@ -14,6 +14,32 @@ source_util() { source "$(dirname $0)/.bash-utils/$1.bash" 2>/dev/null || util=$
 source_util os
 source_util prompt
 
+print_conflict_diff() {
+  git show "HEAD:$1" |
+    diff - "$2" \
+      --unchanged-group-format='%=' \
+      --old-group-format="<<<<<<< Old%c'\\12'%<=======%c'\\12'>>>>>>> New%c'\\12'" \
+      --new-group-format="<<<<<<< Old%c'\\12'=======%c'\\12'%>>>>>>>> New%c'\\12'" \
+      --changed-group-format="<<<<<<< Old%c'\\12'%<=======%c'\\12'%>>>>>>>> New%c'\\12'"
+}
+
+rmdir_recursive() {
+  cwd=$(pwd)
+  iter=$2
+
+  cd $1
+  while (($iter > 0)); do
+    dir=$(basename -- $(pwd))
+    cd ..
+    rmdir "$dir" 2>/dev/null
+    if [[ ! $? -eq 0 ]]; then
+      iter=0
+    else
+      ((iter--))
+    fi
+  done
+}
+
 case $1 in
   s)
     exec git status -s
@@ -28,48 +54,85 @@ case $1 in
     exec git checkout stash@{0} -- "$2"
   ;;
   retrospect)
+    git_dir=$(realpath $(git rev-parse --git-dir))
+    retrospect_dir="${git_dir}/retrospect"
+
+    if [[ -z $2 ]]; then
+      if [[ ! -d "$retrospect_dir" ]]; then
+        echo "No files are being retrospected"
+      else
+        printf "Files currently being retrospected:\n\n"
+        cd "$retrospect_dir" && find * -type f
+      fi
+      exit
+    fi
+
+    dir=$(dirname "$2" 2>/dev/null)
+    if [[ ! $? -eq 0 ]]; then
+      echo "'$2' is not a valid path"
+      exit 1
+    fi
+    if [[ -d "$2" ]]; then
+      echo "git retrospect does not work with directories"
+      exit 1
+    fi
+
+    relative_dir=$(realpath --relative-to="$git_dir/.." "$dir")
+    new_dir=$retrospect_dir
+    if [[ "$relative_dir" == . ]]; then
+      relative_dir=
+      depth=1
+    else
+      new_dir="${new_dir}/${relative_dir}"
+      depth=$(($(res="${relative_dir//[^\/]}"; echo ${#res})+2))
+    fi
+
+    mkdir -p "$new_dir"
+    if [[ ! $? -eq 0 ]]; then
+      exit $?
+    fi
+
+    filename=$(basename -- "$2")
+    new_path="${new_dir}/${filename}"
+
+    if [[ $3 == --abort ]]; then
+      if [[ ! -f "$new_path" ]]; then
+        echo "'$2' is not found in retrospect cache"
+        exit 1
+      fi
+      mv "$new_path" "$2" && rmdir_recursive "$new_dir" $depth
+      exit
+    fi
+
     if [[ ! -f "$2" ]]; then
       echo "'$2' did not match any files"
       exit 1
     fi
 
-    filename=$(basename -- "$2")
-    extension=$([[ "$filename" = *.* ]] && echo ".${filename##*.}")
-    basepath=$([[ -n $extension ]] && echo "${2%.*}" || echo $2)
-    new_path="${basepath}.new${extension}"
+    if grep -q "^<<<<<<<" "$2"; then
+      echo "'$2' has unresolved conflicts that have to be resolved first"
+      exit 1
+    fi
 
     if [[ $3 == --done ]]; then
-      if ! command git diff --quiet "$2"; then
-        prompt_for_bool overwrite "'$2' contains unstaged changes. Overwrite?"
-        if [[ $overwrite == false ]]; then
-          exit
-        fi
-      fi
-      mv "$new_path" "$2"
+      git add "$2" && mv "$new_path" "$2" && rmdir_recursive "$new_dir" $depth
       exit $?
-    else
-      if [[ $3 != --continue ]]; then
-        if [[ -f "$new_path" ]]; then
-          prompt_for_bool overwrite "Overwrite ${new_path}?"
-          if [[ $overwrite == false ]]; then
-            exit
-          fi
-        fi
+    fi
 
-        mv "$2" "$new_path"
-        command git checkout -- "$2"
+    if command git diff --quiet "$2"; then
+      echo "'$2' has no unstaged changes"
+      exit 1
+    fi
+
+    if [[ -f "$new_path" ]]; then
+      prompt_for_bool overwrite "Overwrite ${filename} in retrospect cache (all changes will be lost)?"
+      if [[ $overwrite == false ]]; then
+        exit
       fi
     fi
 
-    if is_macos; then
-      if command -v colordiff 1>/dev/null 2>&1; then
-        exec colordiff -u "$2" "$new_path" | less -r
-      else
-        exec diff -u "$2" "$new_path" | less -r
-      fi
-    else
-      exec diff -u --color=always "$2" "$new_path" | less -r
-    fi
+    cp "$2" "$new_path"
+    print_conflict_diff "$2" "$new_path" > "$2"
   ;;
 
   *)
